6427d538aac444bc13c70c9e063621e16ae6ee70
yuvraj-0777 <yuvsinh0777@gmail.com>
2023-09-21 21:14:33 +0530

    Home Pagge

--- ssportal/src/App.js
+++ ssportal/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import React from 'react';
 import Navbar from './components/navbar';
+import Home from './components/home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <Home />
     </React.Fragment>
   );
 }
---
+++ ssportal/src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+    <main>
+    <button className="buttn-test">
+       <a> TEST YOURSELF NOW!! </a>
+      </button>
+    </main>
+    </>
+  );
+}
+
+export default Home;
